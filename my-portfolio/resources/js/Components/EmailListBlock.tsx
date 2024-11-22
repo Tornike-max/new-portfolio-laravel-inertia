d6dbe56c5a0652fedb396487f0a53d0dd8a13a64
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-11-22 16:06:27 +0400

    i updated my dashboard page

--- my-portfolio/resources/js/Components/EmailListBlock.tsx
+++ my-portfolio/resources/js/Components/EmailListBlock.tsx
@@ -1,34 +1,55 @@
-import React from "react";
 import Block from "./Block";
-import { FiMail } from "react-icons/fi";
+
+const stats = [
+    {
+        data: "2 Years",
+        title: "Experience",
+    },
+    {
+        data: "50+",
+        title: "Projects",
+    },
+    {
+        data: "10+",
+        title: "Technologies Mastered",
+    },
+    {
+        data: "500+",
+        title: "Commits",
+    },
+];
 
 const EmailListBlock = ({ isDark }: { isDark: boolean }) => (
     <Block className="col-span-12 md:col-span-9">
-        <p className="mb-3 text-lg">Join my mailing list</p>
-        <form
-            onSubmit={(e) => e.preventDefault()}
-            className="flex items-center gap-2"
-        >
-            <input
-                type="email"
-                placeholder="Enter your email"
-                className={`w-full rounded border ${
-                    isDark
-                        ? "border-zinc-700 bg-zinc-800 focus:border-red-300"
-                        : "border-zinc-500 bg-zinc-200 text-zinc-800 focus:border-indigo-300"
-                }  px-3 py-1.5 transition-colors  focus:outline-0`}
-            />
-            <button
-                type="submit"
-                className={`flex items-center gap-2 whitespace-nowrap rounded ${
-                    isDark
-                        ? "bg-zinc-50 text-zinc-900 hover:bg-zinc-300"
-                        : "bg-zinc-900 text-zinc-200 hover:bg-zinc-800"
-                }  px-3 py-2 text-sm font-medium transition-colors `}
-            >
-                <FiMail /> Join the list
-            </button>
-        </form>
+        <div className="w-full mx-auto px-4 md:px-8">
+            <ul className="flex flex-col gap-8 items-center justify-center sm:flex-row">
+                {stats.map((item, idx) => (
+                    <li
+                        key={idx}
+                        className={`w-full text-center border hover:scale-105 hover:rotate-2 duration-250 transition-all ${
+                            isDark
+                                ? "bg-zinc-800 border-zinc-700"
+                                : "bg-zinc-100 border-zinc-200"
+                        } px-12 py-4 rounded-lg sm:w-auto shadow-lg hover:shadow-2xl`}
+                    >
+                        <h4
+                            className={`text-4xl ${
+                                isDark ? "text-zinc-200" : "text-zinc-800"
+                            } font-semibold`}
+                        >
+                            {item.data}
+                        </h4>
+                        <p
+                            className={`mt-3 ${
+                                isDark ? "text-zinc-400" : "text-zinc-500"
+                            }  font-medium`}
+                        >
+                            {item.title}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     </Block>
 );
 
--- my-portfolio/resources/js/Components/LocationBlock.tsx
+++ my-portfolio/resources/js/Components/LocationBlock.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import Block from "./Block";
 import { FiMapPin } from "react-icons/fi";
+import { FaPhone } from "react-icons/fa";
+import { Link } from "@inertiajs/react";
 
 const LocationBlock = ({ isDark }: { isDark: boolean }) => (
-    <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
-        <FiMapPin
-            className={`text-3xl ${isDark ? "text-zinc-400" : "text-zinc-800"}`}
-        />
-        <p
-            className={`text-center text-lg ${
-                isDark ? "text-zinc-400" : "text-zinc-700"
-            } `}
+    <Block className="col-span-12 flex flex-col items-center justify-center gap-4 md:col-span-3 group">
+        <Link
+            href={route("contact.index")}
+            className="w-full flex justify-center items-center flex-col gap-3 cursor-pointer"
         >
-            Cyberspace
-        </p>
+            <FaPhone
+                className={`text-3xl ${
+                    isDark ? "text-zinc-400" : "text-zinc-800"
+                } group-hover:rotate-12 duration-250 transition-all`}
+            />
+            <p
+                className={`text-center text-lg ${
+                    isDark ? "text-zinc-400" : "text-zinc-700"
+                }`}
+            >
+                Contact
+            </p>
+        </Link>
     </Block>
 );
 
